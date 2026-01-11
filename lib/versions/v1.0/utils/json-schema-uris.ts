b23d49099ea7eab679ab1d3063e9a06107feacc5
Wesnydy Ribeiro <wesnydy@gmail.com>
2026-01-11 12:39:02 -0300

    refactor: spliting JSON schemas by domain to improve reusability and testability

--- lib/versions/v1.0/utils/json-schema-uris.ts
+++ lib/versions/v1.0/utils/json-schema-uris.ts
@@ -1,6 +1,10 @@
 const JSONSchemaURIs = {
   boletimURI: 'http://www.example.org/boletim.schema.json',
   boletimTypesURI: 'http://www.example.org/boletim-types.schema.json',
+  academicoTypesURI: 'http://www.example.org/academico-types.schema.json',
+  institucionalTypesURI: 'http://www.example.org/institucional-types.schema.json',
+  localizacaoTypesURI: 'http://www.example.org/localizacao-types.schema.json',
+  pessoalTypesURI: 'http://www.example.org/pessoal-types.schema.json',
   simpleTypesURI: 'http://www.example.org/simple-types.schema.json',
 };
 
